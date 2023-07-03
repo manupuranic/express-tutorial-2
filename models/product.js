514b3f0fdcce126952582e4b9cdfe333a32450db
manupuranic <manupuranic@gmail.com>
2023-07-03 16:13:17 +0530

    Models for products

---
+++ models/product.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const rootDir = require("../util/path");
+const fs = require("fs");
+
+const p = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, data) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(data));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
